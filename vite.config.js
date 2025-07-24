d25023d3c9617b62968af8c35dae8fc09b43f298
krishna <krishnagun945@gmail.com>
2025-07-24 16:04:29 +0530

    next

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Mern-Authentication/', 
+  base: '/mern-auth/', 
   plugins: [react(), tailwindcss()],
 })
